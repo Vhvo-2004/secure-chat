bc202a462e319a7f8ef91b3725f58cf38755318c
Idkode <66846347+Idkode@users.noreply.github.com>
2025-10-11 23:42:54 -0400

    Backend com conexão
    - Atributos de conexão com banco por variáveis de ambiente
    - .envs necessários para testar sem docker
    - salve pro front-end (:

---
+++ backend/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const get_prop = (property: string): string => configService.get<string>(property) ?? "";
+
+        const host = get_prop('DATABASE_HOST');
+        const port = get_prop('DATABASE_PORT');
+        const user = get_prop('DATABASE_USER');
+        const pass = encodeURIComponent(get_prop('DATABASE_PASSWORD'));
+        const dbName = get_prop('DATABASE_NAME');
+        return {
+          uri: `mongodb://${user}:${pass}@${host}:${port}/${dbName}?authSource=${dbName}`};
+      },
+      inject: [ConfigService]
+    })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
